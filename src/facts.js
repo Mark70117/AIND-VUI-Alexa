a50f8c73d91fbcda60fdecb1599ee81bd5db9feb
Mark Anderson <mranderso@Marks-Mac-mini.local>
2017-08-23 10:25:55 -0500

    feat: Part 1 complete. 10 fact. 15 utterances

--- src/facts.js
+++ src/facts.js
@@ -1,5 +1,7 @@
 // TODO Add more or replace entirely the facts below for your chosen history domain
 // Include at least 10 distinct facts with a 4-digit year included in the text for each
+// https://didyouknow.org/ai/
+// https://en.wikipedia.org/wiki/Timeline_of_artificial_intelligence
 module.exports = {
     "FACTS_EN": [
         "The field of AI is considered to have its origin in 1950, with publication of British mathematician Alan Turing's paper, Computing Machinery and Intelligence.",
@@ -7,6 +9,14 @@ module.exports = {
         "In 1997, a chess-playing program named Deep Blue, developed by IBM, beat the reigning world chess champion.",
         "A so-called AI Winter occurred in 1974, when funding was cut after Speech Understanding research did not live up to its promise.",
         "A driverless robotic car named Stanley, engineered by Sebastian Thrun's Stanford Racing team, sped through the Mojave desert at 22 miles per hour to win the 2005 Darpa Grand Challenge.",
-        "Replace me and add some more facts!"
+        "The word 'robot' comes from the Czech robota, which means labor. Playwright Karel Capek introduced the word robot in his 1920 play R.U.R. – Rossum’s Universal Robots.",
+        "The General Problem Solver (GPS) was demonstrated by Newell, Shaw and Simon in 1957 while at CMU.",
+        "In 1961 James Slagle wrote the first symbolic integration program, SAINT, which solved calculus problmes at the level of a first year college student.",
+        "Edward Feigenbaum and Julian Feldman published Computers and Thought in 1963, the first collection of articles about artificial intelligence",
+        "Joseph Weizenbaum build ELIZA in 1965, an interactive program that carries on a dialog in English language in any topic.",
+        "McCarthy and Hayes started the discussion about the frame problem with their 1969 essay 'Some Philosophical Problems from the Standpoint of Artificial Intelligence'",
+        "Prolog programming language was developed in 1972 by Alain Colmerauer.",
+        "BKG, a backgammon program written by Hans Berliner at CMU, defeats the reigning world champion in 1979.",
+        "English draughts world champion Tinsley resigned a 1994 match against computer program Chinook.",
     ]
 }
